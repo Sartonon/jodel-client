a1cf9c7364073a243000cf71ad3f9e2928d58720
Santeri Rusila <santtu_1993@hotmail.com>
2018-03-17 23:00:31 +0200

    Time and quote

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import axios from 'axios';
 import upIcon from './icons/ic_thumb_up_black_24px.svg';
 import downIcon from './icons/ic_thumb_down_black_24px.svg';
+import clockIcon from './icons/ic_access_time_white_24px.svg';
 import './App.css';
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-class App extends Component {
+class JodelWall extends Component {
   state = {
     messages: [],
-    username: "",
+    username: '',
     usernameConfirmed: false,
-    message: "",
+    message: '',
     color: 0,
   };
 
@@ -23,100 +26,131 @@ class App extends Component {
   }
 
   initWebSocket = () => {
-    this.websocket = new WebSocket("ws://139.162.254.62/ws2");
+    this.websocket = new WebSocket('ws://localhost:8081/');
     this.websocket.onmessage = this.handleMessage;
     this.websocket.onerror = this.handleError;
     this.websocket.onclose = this.handleOnClose;
   };
 
   getMessages = async () => {
-    const { data } = await axios.get("http://139.162.254.62/jodel/api/messages");
-    console.log(data);
+    const { data } = await axios.get('http://localhost:3010/api/messages');
     this.setState({ messages: data });
   };
 
   handleOnClose = () => {
-    console.log("Connection closed, starting new one...");
     this.initWebSocket();
   };
 
-  handleError = (error) => {
-    console.log("error: ", error);
+  handleError = error => {
+    console.log('error: ', error);
   };
 
-  handleMessage = (e) => {
+  handleMessage = e => {
     const data = JSON.parse(e.data);
-    console.log(data);
-    if (data.type === "jodel") {
-      this.setState({ messages: [ data, ...this.state.messages ] });
-    } else if (data.type === "vote") {
+    if (data.type === 'jodel') {
+      this.setState({ messages: [data, ...this.state.messages] });
+    } else if (data.type === 'vote') {
       this.setState({
         messages: this.state.messages.map(m => {
           if (m.id === data.data.id) {
             return data.data;
           }
           return m;
-        })
+        }),
       });
     }
   };
 
-  sendMessage = (e) => {
+  sendMessage = e => {
+    console.log(moment().format('DD.MM.YYYY HH:mm'));
     e.preventDefault();
-    this.websocket.send(JSON.stringify({
-      type: 'jodel',
-      name: this.state.username,
-      message: this.state.message,
-      color: `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`,
-    }));
-    this.setState({ message: "" });
+    this.websocket.send(
+      JSON.stringify({
+        type: 'jodel',
+        name: this.state.username,
+        message: this.state.message,
+        color: `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(
+          255,
+        )})`,
+        time: moment().format('DD.MM.YYYY HH:mm'),
+      }),
+    );
+    this.setState({ message: '' });
   };
 
-  handleMessageChange = (e) => {
+  handleMessageChange = e => {
     this.setState({ message: e.target.value });
   };
 
-  voteUp = (id) => {
-    this.websocket.send(JSON.stringify({
-      type: 'upvote',
-      id
-    }));
+  voteUp = id => {
+    this.websocket.send(
+      JSON.stringify({
+        type: 'upvote',
+        id,
+      }),
+    );
   };
 
-  voteDown = (id) => {
-    this.websocket.send(JSON.stringify({
-      type: 'downvote',
-      id
-    }));
+  voteDown = id => {
+    this.websocket.send(
+      JSON.stringify({
+        type: 'downvote',
+        id,
+      }),
+    );
   };
 
   renderMessages = () => {
-    console.log("moi");
     return this.state.messages.map(message => {
       return (
-        <div className="Message-wrapper" key={message.id} style={{ backgroundColor: message.color }}>
+        <div
+          className="Message-wrapper"
+          key={message.id}
+          style={{ backgroundColor: message.color }}
+          // onClick={() => this.props.history.push(`/post/${message.id}`)}
+        >
           <div className="Block-wrapper">
             <div
               className="Message-block"
               style={{
-                color: "white"
+                color: 'white',
               }}
             >
-              <div className="Message-name">
-                {message.name}
+              <div className="Message-time">
+                <img className="Clock-icon" src={clockIcon} alt="clock" />
+                <span>
+                  {message.time}
+                </span>
               </div>
               <div className="Message-content">{message.message}</div>
+              {message.quoteText ? (
+                <div className="Message-quote">{`"${message.quoteText}"`} - {message.quoteAuthor}</div>
+              ) : <div className="Message-quote">Ei lainausta tällä kertaa.</div>}
             </div>
             <div className="Message-vote">
-              <img src={upIcon} alt="up" onClick={() => this.voteUp(message.id)} />
-              <div style={{ color: "white", fontSize: "20px" }}>{message.votes}</div>
+              <img
+                src={upIcon}
+                alt="up"
+                onClick={e => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  this.voteUp(message.id);
+                }}
+              />
+              <div style={{ color: 'white', fontSize: '20px' }}>
+                {message.votes}
+              </div>
               <img
                 src={downIcon}
                 style={{
-                  marginTop: '3px'
+                  marginTop: '3px',
                 }}
                 alt="down"
-                onClick={() => this.voteDown(message.id)}
+                onClick={e => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  this.voteDown(message.id);
+                }}
               />
             </div>
           </div>
@@ -128,23 +162,43 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Jodel</h1>
-        </header>
         <div className="Chat-window">
           <div id="chatwindow" className="Message-div">
             {this.renderMessages()}
           </div>
-          <form onSubmit={this.sendMessage}>
-            <div className="Chat-input">
-              <input className="Chat-inputfield" onChange={this.handleMessageChange} value={this.state.message} type="text" />
-              <div className="Send-button" onClick={this.sendMessage}>Lähetä</div>
+          <div className="Chat-input">
+            <input
+              className="Chat-inputfield"
+              onChange={this.handleMessageChange}
+              value={this.state.message}
+              type="text"
+            />
+            <div className="Send-button" onClick={this.sendMessage}>
+              Lähetä
             </div>
-          </form>
+          </div>
         </div>
       </div>
     );
   }
 }
+
+const Post = props => {
+  return <div>Testi</div>;
+};
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Jodel</h1>
+        </header>
+        <Route exact path="/" component={JodelWall} />
+        <Route exact path="/post/:id" component={Post} />
+      </div>
+    </Router>
+  );
+};
 
 export default App;
